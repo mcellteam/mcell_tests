e1f8d4a4f1e3dd7dcd345ae0367c70fca72e8a84
ahusar <adam@adam-husar.com>
2020-01-14 09:56:50 -0800

    Added check for python override

--- scripts/test_utils.py
+++ scripts/test_utils.py
@@ -47,6 +47,9 @@ class ToolPaths:
         
         if opts.python_binary_override:
             self.python_binary = opts.python_binary_override
+            if not os.path.exists(self.python_binary):
+                fatal_error("Python binary from installed bundle ws not found: '" + self.python_binary + "'.")
+                
         else:
             self.python_binary = DEFAULT_PYTHON_BINARY            
             
