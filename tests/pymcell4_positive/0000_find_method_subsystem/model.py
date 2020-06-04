6ad5c671b417d0df1a70b44115517d4195029aff
Adam Husar <ahusar@salk.edu>
2020-06-04 13:55:56 -0700

    Added tests for find_* method

---
+++ tests/pymcell4_positive/0000_find_method_subsystem/model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# based on mcell_tests/tests/mdl/0320_2_mols_react_in_box_it_10
+# rewritten to look like a generated file 
+
+import sys
+import os
+
+MCELL_DIR = os.environ.get('MCELL_DIR', '')
+if MCELL_DIR:
+    sys.path.append(os.path.join(MCELL_DIR, 'lib'))
+else:
+    print("Error: variable MCELL_DIR that is used to find the mcell library was not set.")
+    sys.exit(1)
+    
+import mcell as m
+
+model = m.Model()
+
+a = m.Species('a', diffusion_constant_3d = 1e-6)
+model.add_species(a)
+s1 = model.find_species('a')
+assert s1 != None and s1.name == 'a'
+s2 = model.find_species('b')
+assert s2 == None 
+
+r = m.ReactionRule('r', [a.inst()], [], 0)
+model.add_reaction_rule(r)
+r1 = model.find_reaction_rule('r')
+assert r1 != None and r1.name == 'r'
+r2 = model.find_reaction_rule('s')
+assert r2 == None 
+
+sc = m.SurfaceClass('sc', type = m.SurfacePropertyType.TRANSPARENT, affected_species = a)
+model.add_surface_class(sc)
+sc1 = model.find_surface_class('sc')
+assert sc1 != None and sc1.name == 'sc'
+sc2 = model.find_surface_class('scx')
+assert sc2 == None
+ 
---
+++ tests/pymcell4_positive/1000_find_method_instantiation_data/model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# based on mcell_tests/tests/mdl/0320_2_mols_react_in_box_it_10
+# rewritten to look like a generated file 
+
+import sys
+import os
+
+MCELL_DIR = os.environ.get('MCELL_DIR', '')
+if MCELL_DIR:
+    sys.path.append(os.path.join(MCELL_DIR, 'lib'))
+else:
+    print("Error: variable MCELL_DIR that is used to find the mcell library was not set.")
+    sys.exit(1)
+    
+import mcell as m
+
+model = m.Model()
+
+a = m.Species('a', diffusion_constant_3d = 1e-6)
+
+rs = m.ReleaseSite('rel_a', a, location = m.Vec3(0, 0, 0), number_to_release = 1)
+model.add_release_site(rs)
+s1 = model.find_release_site('rel_a')
+assert s1 != None and s1.name == 'rel_a'
+s2 = model.find_release_site('rel_b')
+assert s2 == None 
+ 
+g = m.GeometryObject('g', [[0, 0, 0], [1, 1, 1], [0, 0, 1]], [[0, 1, 2]]) 
+model.add_geometry_object(g)
+g1 = model.find_geometry_object('g')
+assert g1 != None and g1.name == 'g'
+g2 = model.find_geometry_object('qx')
+assert g2 == None 
+ 
