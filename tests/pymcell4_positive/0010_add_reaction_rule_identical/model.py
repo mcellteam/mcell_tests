286d2d53558acd1e7fae8539e5e9946dfa919ce2
Adam Husar <ahusar@salk.edu>
2020-06-04 14:05:05 -0700

    Added two test to check adding of reactions

---
+++ tests/pymcell4_positive/0010_add_reaction_rule_identical/model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# based on mcell_tests/tests/mdl/0320_2_mols_react_in_box_it_10
+# rewritten to look like a generated file 
+
+import sys
+import os
+
+MCELL_DIR = os.environ.get('MCELL_DIR', '')
+if MCELL_DIR:
+    sys.path.append(os.path.join(MCELL_DIR, 'lib'))
+else:
+    print("Error: variable MCELL_DIR that is used to find the mcell library was not set.")
+    sys.exit(1)
+    
+import mcell as m
+
+model = m.Model()
+
+a = m.Species('a', diffusion_constant_3d = 1e-6)
+
+r = m.ReactionRule('r', [a.inst()], [], 0)
+model.add_reaction_rule(r)
+
+r2 = m.ReactionRule('r', [a.inst()], [], 0)
+model.add_reaction_rule(r2)
+ 
