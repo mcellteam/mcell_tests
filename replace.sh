6039f4d805451fd06ba87b4e92d40f6625fa5cfd
Adam Husar <ahusar@salk.edu>
2020-10-22 11:12:45 -0700

    Removed auxiliary scripts comited by mistake.

--- replace.sh
+++
@@ -1,6 +0,0 @@
-
-#find . -name "*.py" -type -f -print0 | xargs -0 sed -i 's/element_connections/wall_list/g'
-
-find . -name "*.py" -type f -print0 | xargs -0 sed -i 's/element_connections/wall_list/g'
-
-#find . -type f -print0 | xargs -0 sed -i 's/element_connections/wall_list/g'
--- replace2.sh
+++
@@ -1,2 +0,0 @@
-
-find . \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i 's/mcell_dir/mcell_path/g'
