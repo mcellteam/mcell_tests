27cc7fa573852f8eeadb53d07718814e29a6a8ee
Adam Husar <ahusar@salk.edu>
2020-11-26 10:07:32 -0800

    Added test for checking of minimal unimol rxn time

---
+++ tests/nutmeg4_pymcell4/0500_too_high_unimol_rate/model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+MCELL_PATH = os.environ.get('MCELL_PATH', '')
+if MCELL_PATH:
+    sys.path.append(os.path.join(MCELL_PATH, 'lib'))
+else:
+    print("Error: variable MCELL_PATH that is used to find the mcell library was not set.")
+    sys.exit(1)
+
+import mcell as m
+
+
+params = m.bngl_utils.load_bngl_parameters('test.bngl')
+
+ITERATIONS = int(params['ITERATIONS'])
+
+DUMP = True
+EXPORT_DATA_MODEL = True
+
+
+# ---- load bngl file ----
+
+model = m.Model()
+
+model.load_bngl('test.bngl')
+
+
+# ---- configuration ----
+
+model.config.total_iterations_hint = ITERATIONS 
+
+
+model.initialize()
+model.run_iterations(ITERATIONS)
+model.end_simulation()
