729e227f67ea47c869f5b852f4b18144729b309b
Adam Husar <ahusar@salk.edu>
2020-12-13 10:29:52 -0800

    Added todo test for shorter error message

---
+++ tests/nutmeg4_pymcell4/2000_shorter_error_message/model.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+MCELL_PATH = os.environ.get('MCELL_PATH', '')
+if MCELL_PATH:
+    sys.path.append(os.path.join(MCELL_PATH, 'lib'))
+else:
+    print("Error: variable MCELL_PATH that is used to find the mcell library was not set.")
+    sys.exit(1)
+
+import geometry
