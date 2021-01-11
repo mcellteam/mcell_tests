e0dc43fbbd4b7cf2a89bb9bcbb6f4acd86542186
Adam Husar <ahusar@salk.edu>
2021-01-11 15:12:37 -0800

    Added checkpoint_iters for bngl tests

--- scripts/tester_bngl_data_model_pymcell4.py
+++ scripts/tester_bngl_data_model_pymcell4.py
@@ -73,6 +73,11 @@ class TesterBnglDataModelPymcell4(TesterDataModelPymcell4):
         if self.is_known_fail():
             return SKIPPED
         
+        if ARG_CHECKPOINTS in self.args:
+            iters = self.load_checkpoint_iters()
+            if not iters:
+                return IGNORED        
+        
         self.clean_and_create_work_dir()
         
         self.copy_exporter_and_test()
@@ -85,6 +90,8 @@ class TesterBnglDataModelPymcell4(TesterDataModelPymcell4):
             extra_args = ['-t']
             if ARG_CONVERT_W_BNGL in self.args:
                 extra_args += ['-b']
+            if ARG_CHECKPOINTS in self.args:
+                extra_args += ['-k', ','.join(iters)]                  
                 
             res = self.run_dm_to_pymcell_conversion(
                 os.path.join(self.test_work_path, 'data_model.json'), 
@@ -96,7 +103,17 @@ class TesterBnglDataModelPymcell4(TesterDataModelPymcell4):
              
         # 3) run test (model.py)
         if res == PASSED:
-            res = self.run_pymcell4(test_dir=self.test_work_path)
+            if ARG_CHECKPOINTS not in self.args:
+                # single run
+                res = self.run_pymcell4(test_dir=self.test_work_path)
+            else:
+                # run with checkpoints, run until run has finished
+                # TODO: use seed argument 
+                res = self.run_pymcell4(test_dir=self.test_work_path)
+                while res == PASSED and \
+                    not self.run_finished(os.path.join(self.test_work_path, 'reports/run_report_00001.txt')):
+                    
+                    res = self.run_pymcell4(test_dir=self.test_work_path)
         
         if self.is_todo_test():
             return TODO_TEST
