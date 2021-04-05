e854fda6e2bcc63cccc8b911508a5a9747aa14e7
Adam Husar <ahusar@salk.edu>
2021-04-05 14:45:06 -0700

    Added test that uses model.config.continue_after_sigalrm = True

---
+++ tests/nutmeg4_pymcell4/2785_schedule_checkpoint_async_w_sigalrm_continue/model.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# test to check that run_iterations returns correct values 
+# and that the simulation is resumed correctly (TODO)
+
+import sys
+import os
+import time
+import threading
+import signal
+
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# ---- import mcell module located in directory ----
+# ---- specified by system variable MCELL_PATH  ----
+MCELL_PATH = os.environ.get('MCELL_PATH', '')
+if MCELL_PATH:
+    lib_path = os.path.join(MCELL_PATH, 'lib')
+    if os.path.exists(os.path.join(lib_path, 'mcell.so')) or \
+        os.path.exists(os.path.join(lib_path, 'mcell.pyd')):
+        sys.path.append(lib_path)
+    else:
+        print("Error: Python module mcell.so or mcell.pyd was not found in "
+              "directory '" + lib_path + "' constructed from system variable "
+              "MCELL_PATH.")
+        sys.exit(1)
+else:
+    print("Error: system variable MCELL_PATH that is used to find the mcell "
+          "library was not set.")
+    sys.exit(1)
+
+import mcell as m
+
+import parameters
+
+# create main model object
+model = m.Model()
+
+model.load_bngl('model.bngl', './react_data/seed_' + str(parameters.SEED).zfill(5) + '/')
+
+# ---- configuration ----
+
+model.config.time_step = parameters.TIME_STEP
+model.config.seed = parameters.SEED
+model.config.total_iterations = parameters.ITERATIONS
+model.config.continue_after_sigalrm = True
+
+model.notifications.rxn_and_species_report = False
+
+model.initialize()
+
+if parameters.DUMP:
+    model.dump_internal_state()
+
+if parameters.EXPORT_DATA_MODEL and model.viz_outputs:
+    model.export_data_model()
+    
+# schedule checkpoint to be run at a given time
+def send_sigalrm(model_arg):
+    print("Sending SIGALRM")
+    os.kill(os.getpid(), signal.SIGALRM)
+
+        
+threading.Timer(2, send_sigalrm, [model]).start()
+
+model.run_iterations(parameters.ITERATIONS)
+
+model.end_simulation()
