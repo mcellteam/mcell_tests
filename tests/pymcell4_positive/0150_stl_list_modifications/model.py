b436ac28f785122ddc06419a8baf3c3a23dc509f
Adam Husar <ahusar@salk.edu>
2021-05-01 08:48:55 -0700

    Added check for conversion of tuples and a double vector modifications

--- tests/pymcell4_positive/0150_stl_list_modifications/model.py
+++ tests/pymcell4_positive/0150_stl_list_modifications/model.py
@@ -13,9 +13,26 @@ import mcell as m
 
 # checking that .append and other modifications works
 ct_u = m.ComponentType('u', states = ['0'])
+
+#print(type(ct_u.states))
 assert len(ct_u.states) == 1
 ct_u.states.append('1')
 assert len(ct_u.states) == 2
 
 ct_u.states[1] = '2'
 assert ct_u.states[1] == '2' 
+
+
+rs = m.ReleaseSite(
+    name = 'test',
+    complex = m.Complex('A'),
+    shape = m.Shape.SPHERICAL,
+    location = (1, 2, 3),
+    number_to_release = 1000
+)
+
+#print(type(rs.location))
+#print(rs.location)
+rs.location[0] = 2
+#print(rs.location)
+assert rs.location[0] == 2
