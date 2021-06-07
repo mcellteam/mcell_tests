37ebb007f39fa5643763c072d6a003d77492de3b
Adam Husar <ahusar@salk.edu>
2021-06-07 11:33:44 -0700

    Added benchamrk based on autophosphorylation model

---
+++ benchmarks/pymcell4/B5000_autophosphorylation/subsystem.py
@@ -0,0 +1,19 @@
+# WARNING: This is an automatically generated file and will be overwritten
+#          by CellBlender on the next model export.
+
+import os
+import shared
+import mcell as m
+
+from parameters import *
+
+# ---- subsystem ----
+
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# ---- create subsystem object and add components ----
+
+subsystem = m.Subsystem()
+
+# load subsystem information from bngl file
+subsystem.load_bngl_molecule_types_and_reaction_rules(os.path.join(MODEL_PATH, 'model.bngl'), shared.parameter_overrides)
