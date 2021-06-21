9abe72344e1d35c17b214b961ba7378d1ee34845
Adam Husar <ahusar@salk.edu>
2021-06-21 10:03:38 -0700

    Added test to check that reactants are printed with the Some reactions were missed message

---
+++ tests/nutmeg4_pymcell4/1300_surf_rxns_missed/model.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+MCELL_PATH = os.environ.get('MCELL_PATH', '')
+if MCELL_PATH:
+    sys.path.append(os.path.join(MCELL_PATH, 'lib'))
+else:
+    print("Error: variable MCELL_PATH that is used to find the mcell library was not set.")
+    sys.exit(1)
+
+import mcell as m
+
+SEED = 1
+bngl_file = 'surf_surf.bngl'  
+
+
+params = m.bngl_utils.load_bngl_parameters(bngl_file)
+
+ITERATIONS = int(params['ITERATIONS'])
+    
+if 'MCELL_TIME_STEP' in params:
+    TIME_STEP = float(params['MCELL_TIME_STEP'])
+else:
+    TIME_STEP = 1e-6 
+DUMP = True
+EXPORT_DATA_MODEL = True
+
+
+# ---- load bngl file ----
+
+model = m.Model()
+
+if 'MCELL_DEFAULT_COMPARTMENT_VOLUME' in params:
+    MCELL_DEFAULT_COMPARTMENT_VOLUME = params['MCELL_DEFAULT_COMPARTMENT_VOLUME']
+    MCELL_DEFAULT_COMPARTMENT_EDGE_LENGTH = MCELL_DEFAULT_COMPARTMENT_VOLUME**(1.0/3.0) 
+    default_compartment = m.geometry_utils.create_box(
+        'default_compartment', MCELL_DEFAULT_COMPARTMENT_EDGE_LENGTH
+    )
+    model.add_geometry_object(default_compartment)
+else:
+    MCELL_DEFAULT_COMPARTMENT_EDGE_LENGTH = 1
+    default_compartment = None
+    
+viz_output = m.VizOutput(
+    mode = m.VizMode.ASCII,
+    output_files_prefix = './viz_data/seed_' + str(SEED).zfill(5) + '/Scene',
+    every_n_timesteps = 5000
+)
+model.add_viz_output(viz_output)
+
+gdat_file = ''
+if 'GDAT' in params and params['GDAT'] == 1:
+    gdat_file = os.path.splitext(bngl_file)[0] + '.gdat'
+
+model.load_bngl(bngl_file, './react_data/seed_' + str(SEED).zfill(5) + '/' + gdat_file, default_compartment)
+
+
+# ---- configuration ----
+
+model.config.time_step = TIME_STEP
+model.config.seed = SEED
+model.config.total_iterations = ITERATIONS 
+
+model.config.partition_dimension = MCELL_DEFAULT_COMPARTMENT_EDGE_LENGTH
+model.config.subpartition_dimension = MCELL_DEFAULT_COMPARTMENT_EDGE_LENGTH 
+
+
+model.initialize()
+
+if DUMP:
+    model.dump_internal_state()
+
+if EXPORT_DATA_MODEL and model.viz_outputs:
+    model.export_data_model()
+
+model.run_iterations(ITERATIONS)
+model.end_simulation()
