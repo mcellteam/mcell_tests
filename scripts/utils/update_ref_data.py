3b44aa088935c4825de28c5bdfb9e7736ee644b1
Adam Husar <ahusar@salk.edu>
2021-06-27 10:34:01 -0700

    Updated Fceri tests after fix of EC compartment volume
    bngl/1555 is set as TODO and needs to be explored further

--- scripts/utils/update_ref_data.py
+++ scripts/utils/update_ref_data.py
@@ -2,6 +2,8 @@ import os
 import sys
 import shutil
 
+#SEED_DIR = 'seed_00001'
+SEED_DIR = 'seed_56362'
 
 def update_dir(work_data_dir, test_data_dir):
     files = os.listdir(test_data_dir)
@@ -12,11 +14,11 @@ def update_dir(work_data_dir, test_data_dir):
 def update_ref_data(work_dir, test_dir):
     print("Updating MCell4 ref data in " + test_dir + " using data from " + work_dir)
 
-    work_viz_seed = os.path.join(work_dir, 'viz_data', 'seed_00001');
-    test_viz_seed = os.path.join(test_dir, 'ref_viz_data_4', 'seed_00001');
+    work_viz_seed = os.path.join(work_dir, 'viz_data', SEED_DIR);
+    test_viz_seed = os.path.join(test_dir, 'ref_viz_data_4', SEED_DIR);
 
-    work_react_seed = os.path.join(work_dir, 'react_data', 'seed_00001');
-    test_react_seed = os.path.join(test_dir, 'ref_react_data_4', 'seed_00001');
+    work_react_seed = os.path.join(work_dir, 'react_data', SEED_DIR);
+    test_react_seed = os.path.join(test_dir, 'ref_react_data_4', SEED_DIR);
     
     update_dir(work_viz_seed, test_viz_seed)
     
