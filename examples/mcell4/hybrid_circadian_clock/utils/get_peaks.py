fb2deafd528e862b4546a4c30a3493c2f9070322
Adam Husar <ahusar@salk.edu>
2021-08-20 15:50:40 -0700

    Added low pass filter application before finding peaks

--- examples/mcell4/hybrid_circadian_clock/utils/get_peaks.py
+++ examples/mcell4/hybrid_circadian_clock/utils/get_peaks.py
@@ -4,6 +4,22 @@ import sys
 import os
 from scipy.signal import find_peaks
 
+from scipy.signal import butter, lfilter, freqz
+import matplotlib.pyplot as plt
+
+
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    #print(normal_cutoff)
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
 
 def load_dat_file(file):
     obs_name = os.path.splitext(os.path.basename(file))[0]
@@ -25,7 +41,7 @@ def get_peaks_for_single_obs(df, obs_name, file):
     peaks, props = find_peaks(
         df[obs_name], 
         width=int(len(df)/30),
-        height = 1000.0
+        height = 500.0
     )
     
     #if len(peaks) == 3 or len(peaks) == 4:
@@ -69,9 +85,22 @@ def get_peaks_for_single_obs(df, obs_name, file):
         p1 = times[-1]
     
     return p0, p1, False
+    
         
+def prepare_data(df, obs_name):
+    sf = 100
+    ff = 0.1
+    
+    # run filter in both directions  
+    df[obs_name] = butter_lowpass_filter(df[obs_name], ff, sf, order=5)
+    
+    df[obs_name] = butter_lowpass_filter(df[obs_name].values[::-1], ff, sf, order=5)
+    
+    df[obs_name] = df[obs_name].values[::-1] 
+    return df
 
-def plot_peaks(df, obs_name, p0, p1, seed):
+
+def plot_peaks(df, df_orig, obs_name, p0, p1, seed):
     
     fig,ax = plt.subplots()
     ax.set_title(obs_name)
@@ -79,7 +108,8 @@ def plot_peaks(df, obs_name, p0, p1, seed):
     plt.xlabel("time [s]")
     plt.ylabel(obs_name + "(t)")
     
-    ax.plot(df.index, df[obs_name])
+    ax.plot(df.index, df[obs_name], linewidth=1)
+    ax.plot(df_orig.index, df_orig[obs_name], linewidth=1)
     
     plt.axvline(x=p0, color='r')
     plt.axvline(x=p1, color='r')
@@ -97,6 +127,9 @@ def get_all_peaks(dir):
         if not seed_dir.startswith('seed_'):
             continue
         
+        if not os.path.isdir(os.path.join(dir, seed_dir)):
+            continue
+        
         seed = int(seed_dir[len('seed_'):])
         
         new_row = [seed, 0.0, 0.0, 0.0, 0.0]
@@ -110,17 +143,20 @@ def get_all_peaks(dir):
                 if obs_name != 'A' and obs_name != 'R':
                     continue
                 
+                df_orig = df.copy()
+                df = prepare_data(df, obs_name)
+                
                 p0, p1, skip = get_peaks_for_single_obs(df, obs_name, file_path)
                 
                 skip_row = skip_row or skip 
                 if obs_name == 'A':
                     new_row[1] = p0
                     new_row[2] = p1
-                    plot_peaks(df, obs_name, p0, p1, seed_dir)
+                    plot_peaks(df, df_orig, obs_name, p0, p1, seed_dir)
                 else:
                     new_row[3] = p0
                     new_row[4] = p1
-                    plot_peaks(df, obs_name, p0, p1, seed_dir)
+                    plot_peaks(df, df_orig, obs_name, p0, p1, seed_dir)
                     
         
         a_series = pd.Series(new_row, index = res.columns)
