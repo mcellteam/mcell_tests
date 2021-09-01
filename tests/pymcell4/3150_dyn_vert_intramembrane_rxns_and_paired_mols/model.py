601b494f2d85c9409326e3fa4a43aafae8039712
Adam Husar <ahusar@salk.edu>
2021-09-01 06:32:58 -0700

    Added test that shows 'pull' through paired molecules.

--- tests/pymcell4/3150_dyn_vert_intramembrane_rxns_and_paired_mols/model.py
+++ tests/pymcell4/3150_dyn_vert_intramembrane_rxns_and_paired_mols/model.py
@@ -107,7 +107,7 @@ model.add_viz_output(viz_output)
 
 model.config.time_step = TIME_STEP
 model.config.seed = SEED
-model.config.total_iterations = 10*ITERATIONS
+model.config.total_iterations = ITERATIONS
 
 model.config.partition_dimension = 10
 model.config.subpartition_dimension = 2.5
@@ -149,26 +149,7 @@ def print_molecule_info(model, id, msg):
     print("  " + msg + " (" + str(m.id) + " - " + s + "): " + m.geometry_object.name + ":" + str(m.wall_index))
 
 
-# returns current value
-def dec_counter(counter, object, wall_index):
-    assert wall_index in counter[object]
-    assert counter[object][wall_index] > 0
-    
-    counter[object][wall_index] -= 1
-    return counter[object][wall_index]
-    
-    
-def inc_counter(counter, object, wall_index):
-    if wall_index in counter[object]:
-        counter[object][wall_index] += 1
-    else:
-        counter[object][wall_index] = 1
-        
-    return counter[object][wall_index]
-
-
-def a_plus_b_callback(rxn_info, context):
-    (model, blocked_mols, block_counter) = context
+def a_plus_b_callback(rxn_info, model):
     assert rxn_info.type == m.ReactionType.SURFACE_SURFACE 
     
     assert len(rxn_info.reactant_ids) == 2
@@ -184,85 +165,35 @@ def a_plus_b_callback(rxn_info, context):
     # get the first reactant and block its wall, reac1 may be either A or B
     reac1 = model.get_molecule(rxn_info.reactant_ids[0])
     assert reac1.type == m.MoleculeType.SURFACE
-    
-    w1 = model.get_wall(reac1.geometry_object, reac1.wall_index)
 
-    cnt1 = inc_counter(block_counter, reac1.geometry_object, reac1.wall_index)
-    #print("  blocking wall " + reac1.geometry_object.name + ":" + str(reac1.wall_index) + " (" + str(cnt1) + ")")
-    #w1.is_movable = False
-
-    # get the second reactant and block its wall
-    reac2 = model.get_molecule(rxn_info.reactant_ids[1])
-    assert reac2.type == m.MoleculeType.SURFACE
-    
-    w2 = model.get_wall(reac2.geometry_object, reac2.wall_index)
-    
-    cnt2 = inc_counter(block_counter, reac2.geometry_object, reac2.wall_index)
-    #print("  blocking wall " + reac2.geometry_object.name + ":" + str(reac2.wall_index) + " (" + str(cnt2) + ")")
-    #w2.is_movable = False
-    
-    model.pair_walls(rxn_info.product_ids[0], rxn_info.product_ids[1])
-    
+    # tell MCell that the two molecules are paired and that when 
+    # a wall with one molecule is moved, the second wall is moved as well 
+    model.pair_molecules(rxn_info.product_ids[0], rxn_info.product_ids[1])
     
     # debug printout on locations of products
     print_molecule_info(model, rxn_info.product_ids[0], "product 1")
     print_molecule_info(model, rxn_info.product_ids[1], "product 2")
 
-    # and now add a mapping that remembers ID of molecule C that is 'bound' with molecule 'D',
-    # this is needed to 'unbind them both later 
-    assert len(rxn_info.product_ids) == 2
-    blocked_mols[rxn_info.product_ids[0]] = rxn_info.product_ids[1]
     
 
-def c_to_e_callback(rxn_info, context):
+def c_to_e_callback(rxn_info, model):
     print("** Unbinding of membrane through reaction C -> A and D -> B (" + 
           str(rxn_info.reactant_ids[0]) + ")")
     
     # this callback is called when a unimolecular reaction of C occurs
-    (model, blocked_mols, block_counter) = context
     
     assert len(rxn_info.reactant_ids) == 1
     reactant_C_id = rxn_info.reactant_ids[0] 
     
     # we did not start with any C molecules, the only ones in our system are created 
-    # through the A + B -> C + D reaction, therefore it must be in the mapping created in 
-    # a_plus_b_callback
-    assert reactant_C_id in blocked_mols 
+    # through the A + B -> C + D reaction, therefore C must be a paired molecule
+    # and we get the original D with get_paired_molecule
     
-    reactant_D_id = blocked_mols[reactant_C_id]
+    reactant_D_id = model.get_paired_molecule(reactant_C_id)
+    assert reactant_D_id != m.ID_INVALID
     
     print_molecule_info(model, reactant_C_id, "reactant 1")
     print_molecule_info(model, reactant_D_id, "reactant 2")
-    
-    # forget this mapping
-    del blocked_mols[reactant_C_id]
-    
-    # unblock walls - the molecules are in the same place because walls were not allowed 
-    # to move and molecules C and are not diffusible 
-    mol_C = model.get_molecule(reactant_C_id)
-    mol_D = model.get_molecule(reactant_D_id)
-    
-    assert mol_C.type == m.MoleculeType.SURFACE
-    assert mol_D.type == m.MoleculeType.SURFACE
-    
-    # there might be multiple molecules pairs blocking the wall  
-    
-    w1 = model.get_wall(mol_C.geometry_object, mol_C.wall_index)
-    
-    num_blocked1 = dec_counter(block_counter, mol_C.geometry_object, mol_C.wall_index)
-    if num_blocked1 == 0:
-        print("  unblocking wall " + mol_C.geometry_object.name + ":" + str(mol_C.wall_index))
-        w1.is_movable = True
-
-
-    w2 = model.get_wall(mol_D.geometry_object, mol_D.wall_index)
-    
-    num_blocked2 = dec_counter(block_counter, mol_D.geometry_object, mol_D.wall_index)
-    if num_blocked2 == 0:
-        print("  unblocking wall " + mol_D.geometry_object.name + ":" + str(mol_D.wall_index))
-        w2.is_movable = True
-    
-    # num_blocked1 does nto have to be equal to num_blocked2
     
     # and change D back to B
     products = model.run_reaction(rxn_D_to_B, [reactant_D_id], rxn_info.time)
@@ -274,31 +205,22 @@ def c_to_e_callback(rxn_info, context):
     print_molecule_info(model, products[0], "product 2")
 
 
-# how many times we have blocked a wall
-# count = block_counters[sphere][wall_index]
-block_counters = {}
-block_counters[Sphere1] = {}
-block_counters[Sphere2] = {}
-
-# map: key is id of molecule C and corresponding value is id of molecule D with
-# which it was created
-blocked_mols = {}   
-
 # register reaction callback(s)
 model.register_reaction_callback(
     a_plus_b_callback,
-    (model, blocked_mols, block_counters), 
+    model, 
     rxn_A_plus_B 
 )
 
 model.register_reaction_callback(
     c_to_e_callback, 
-    (model, blocked_mols, block_counters), 
+    model, 
     rxn_C_to_E
 )
 
 # and run simulation    
 for i in range(ITERATIONS):
+    print("IT", i)
     
     # dump datamodel every N iterations
     if i % VIZ_FREQUENCY == 0: 
